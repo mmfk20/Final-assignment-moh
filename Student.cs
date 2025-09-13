3540cd681bd216fa54e96c6646b817368f2edf96
mmfk20 <mmfkr9@hotmail.com>
2025-09-13 11:16:29 -0400

    Create Student.cs

---
+++ Student.cs
@@ -0,0 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentCodeFirst
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        public string LastName { get; set; } = null!;
+
+        public DateTime? EnrolledOn { get; set; }
+    }
+}
